458135c7882f5373fc99ea58d31ed03125f05c02
wastless <uzlikeevaa@gmail.com>
2025-04-07 04:06:12 +0500

    ridex-design-app

--- src/components/canvas/SelectionTools.tsx
+++ src/components/canvas/SelectionTools.tsx
@@ -65,6 +65,60 @@ function SelectionTools({
     [selection],
   );
 
+  const bringForward = useMutation(
+    ({ storage }) => {
+      const liveLayerIds = storage.get("layerIds");
+      const indices: number[] = [];
+
+      const arr = liveLayerIds.toArray();
+
+      for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+
+        if (element !== undefined && selection?.includes(element)) {
+          indices.push(i);
+        }
+      }
+
+      // Sort indices in descending order to move from bottom to top
+      indices.sort((a, b) => b - a);
+
+      for (const index of indices) {
+        if (index < arr.length - 1) {
+          liveLayerIds.move(index, index + 1);
+        }
+      }
+    },
+    [selection],
+  );
+
+  const sendBackward = useMutation(
+    ({ storage }) => {
+      const liveLayerIds = storage.get("layerIds");
+      const indices: number[] = [];
+
+      const arr = liveLayerIds.toArray();
+
+      for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+
+        if (element !== undefined && selection?.includes(element)) {
+          indices.push(i);
+        }
+      }
+
+      // Sort indices in ascending order to move from top to bottom
+      indices.sort((a, b) => a - b);
+
+      for (const index of indices) {
+        if (index > 0) {
+          liveLayerIds.move(index, index - 1);
+        }
+      }
+    },
+    [selection],
+  );
+
   const selectionBounds = useSelectionBounds();
   if (!selectionBounds) {
     return null;
@@ -91,16 +145,33 @@ function SelectionTools({
             onSelect={bringToFront}
             className="flex items-center justify-between"
           >
-            Переместить вперед
+            На передний план
             <Kbd.Root>]</Kbd.Root>
           </Dropdown.Item>
+          <Dropdown.Item
+            onSelect={bringForward}
+            className="flex items-center justify-between"
+          >
+            Переместить вперед
+            <Kbd.Root>Ctrl ]</Kbd.Root>
+          </Dropdown.Item>
+
           <Dropdown.Item
             onSelect={sendToBack}
             className="flex items-center justify-between"
           >
-            Переместить назад
+            На задний план
             <Kbd.Root>[</Kbd.Root>
           </Dropdown.Item>
+
+          <Dropdown.Item
+            onSelect={sendBackward}
+            className="flex items-center justify-between"
+          >
+            Переместить назад
+            <Kbd.Root>Ctrl [</Kbd.Root>
+          </Dropdown.Item>
+          
         </Dropdown.Group>
       </Dropdown.Content>
     </Dropdown.Root>
